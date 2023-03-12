c658123a357d7f83c44a34ae273a09a4c78c9551
Ajibolatao1 <taolawbal4@gmail.com>
2023-03-12 18:29:43 +0100

    secret added

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,10 +5,10 @@ import GithubProvider from "next-auth/providers/github";
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID,
-    //   clientSecret: process.env.GOOGLE_SECRET,
-    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
